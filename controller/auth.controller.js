70872716be948a41a53bc11c229931d3c1b4c5f7
Prashant-Pawale <prashantpawale643@gmail.com>
2022-11-26 22:00:05 +0530

    signin

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,55 +1,71 @@
 let bcrypt = require("bcryptjs");
-const config  = require("./../config/auth.config");
+const config = require("./../config/auth.config");
 let jwt = require("jsonwebtoken");
 const { Sequelize } = require("sequelize");
-let db = require("./../model/index");   
+let db = require("./../model/index");
 let User = db.user; // user model which we imported in index file of model.
-let Roles = db.roles // roles model 
+let Roles = db.roles; // roles model
 
 let signup = async (req, res, next) => {
-    let user = await User.create({
-        username : req.body.username,
-        email : req.body.email,
-        password : bcrypt.hashSync(req.body.password, 8)
+  let user = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8),
+  });
+  // req.body.roles will be an array like ['admin','user']
+  if (req.body.roles) {
+    let roles = await Roles.findAll({
+      where: { name: { [Sequelize.Op.or]: req.body.roles } },
     });
-    // req.body.roles will be an array like ['admin','user']
-    if(req.body.roles){
-        let roles = await Roles.findAll({
-            where : {name : {[Sequelize.Op.or] : req.body.roles}}
-        });
-
-        await user.setRoles(roles);
-        res.status(200).json({
-            message : "User registered successfully"
-        });
-    }
-}
+    // console.log(roles) // roles is an obj with id and name as key
+    await user.setRoles(roles);
+    res.status(200).json({
+      message: "User registered successfully",
+    });
+  }
+};
 
 let signin = async (req, res, next) => {
-    let userName = await User.findOne({
-        where : {username : req.body.username}
+  let userName = await User.findOne({
+    where: { username: req.body.username },
+  });
+//   console.log(userName); // it prints an object having username same as provided in req.body
+
+  if (!userName) {
+    res.status(404).json({
+      message: "User not found",
     });
+    return;
+  }
 
-    if(!userName) {
-        res.status(404).json({
-            message : "User not found"
-        });
-        return;
-    }
-
-    let isValidPassword = bcrypt.compareSync(
-                            re.body.password, 
-                            userName.password);
-    if(!isValidPassword){
-        res.status(401).json({
-            message : "Password is Incorrect"
-        });
-        return;
-    }
-    // if password is valid 
-    let token = jwt.sign({id : userName.id}, config.secret, {
-        expiresIn : 86400,
+  let isValidPassword = bcrypt.compareSync(
+    req.body.password,
+    userName.password
+  );
+  if (!isValidPassword) {
+    res.status(401).json({
+      message: "Password is Incorrect",
     });
-}
+    return;
+  }
+  // if password is valid
+  let token = jwt.sign({ id: userName.id }, config.secret, {
+    expiresIn: 86400,
+  });
+  let authorities = [];
+  let roles = await userName.getRoles();
+//   console.log(roles); // roles is an obj with id and name as key
+  for (let i = 0; i < roles.length; i++) {
+    authorities.push("ROLE_" + roles[i].name.toUpperCase());
+  }
 
-module.exports = { signup, signin }
+  res.status(200).send({
+    id: userName.id,
+    username: userName.username,
+    email: userName.email,
+    roles: authorities,
+    accessToken: token,
+  });
+};
+
+module.exports = { signup, signin };
